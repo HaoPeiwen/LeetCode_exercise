b2cee42f05edcb7867a4016fd82b1e95aa30f402
Micheal Yang <never_say_never@sjtu.edu.cn>
2017-09-29 14:08:32 +0800

    Create readme.m

---
+++ code source/readme.m
@@ -0,0 +1,5 @@
+# 2017/9/29
+I find a platform, called Leetcode, to practice coding im my space time.
+Here are the source files in Python that I wrtie for each problem.
+To run these files, you must have been installed some Python libs, such as
+numpy, pandas...
